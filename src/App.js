ffd4a1023dc365a7770fafc2b8418e1f624b289c
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-08-12 17:34:25 +0300

    add localStorage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+const loadJSON = (key) => key && JSON.parse(localStorage.getItem(key));
+const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify(data));
 
 const GitHubUser = ({ login }) => {
-  const [data, setData] = useState();
+  const [data, setData] = useState(loadJSON(`user:${login}`));
 
   useEffect(() => {
-    if (!login) return
+    if (!data) return;
+    if (data.login === login) return;
+    const { name, avatar_url, location } = data;
+    saveJSON(`user:${login}`, {
+      name,
+      login,
+      avatar_url,
+      location,
+    });
+  }, [data]);
+
+  useEffect(() => {
+    if (!login) return;
+    if (data && data.login === login) return;
     fetch(`http://api.github.com/users/${login}`)
-      .then(res => res.json())
+      .then((res) => res.json())
       .then(setData)
       .catch(console.error);
-  }, [login])
+  }, [login]);
 
-  if (data)
-    return <pre>{JSON.stringify(data, null, 2)}</pre>
+  if (data) return <pre>{JSON.stringify(data, null, 2)}</pre>;
 
   return null;
 };
 
 function App() {
-  return <GitHubUser login={"moonhighway"} />;
+  return <GitHubUser login={"victorizbitskiy"} />;
 }
 
 export default App;
