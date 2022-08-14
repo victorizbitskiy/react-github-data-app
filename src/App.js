2b088b37120d8521c72b3d3a9d36435662131ef7
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-08-14 13:48:45 +0300

    add loading...

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify(data));
 
 const GitHubUser = ({ login }) => {
   const [data, setData] = useState(loadJSON(`user:${login}`));
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (!data) return;
-    // if (data.login === login) return;
     const { name, avatar_url, location } = data;
     saveJSON(`user:${login}`, {
       name,
@@ -22,21 +23,36 @@ const GitHubUser = ({ login }) => {
 
   useEffect(() => {
     if (!login) return;
-    if (data && data.login === login) return;
+    setLoading(true);
+
     fetch(`http://api.github.com/users/${login}`)
       .then((res) => res.json())
       .then(setData)
-      .catch(console.error);
+      .then(() => setLoading(false))
+      .catch(setError);
   }, [login]);
 
-  if (data) return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  if (!data) return null;
 
-  return null;
+  return (
+    <div className="githubUser">
+      <img src={data.avatar_url} alt={data.login} style={{ width: 200 }} />
+      <div>
+        <h1>{data.login}</h1>
+        {data.name && <p>{data.name}</p>}
+        {data.location && <p>{data.location}</p>}
+      </div>
+    </div>
+  );
 };
 
 function App() {
   console.log("App");
-  return <GitHubUser login={"moonhighway"} />; // moonhighway  victorizbitskiy objectionary
+  return <GitHubUser login={"victorizbitskiy"} />; // moonhighway  victorizbitskiy objectionary
 }
 
 export default App;
