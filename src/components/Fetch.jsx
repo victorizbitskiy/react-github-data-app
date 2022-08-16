d73119003be004538883c7f8e83d4634c8c9cb89
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-08-16 16:13:18 +0300

    refactoring

---
+++ src/components/Fetch.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import useFetch from '../hooks/useFetch';
+
+const Fetch = ({
+  uri,
+  renderSuccess,
+  loadingFallback = <p>loading...</p>,
+  renderError = error => (
+    <pre>{JSON.stringify(error, null, 2)}</pre>
+  )
+}) => {
+  const { loading, data, error } = useFetch(uri);
+  if (loading) return loadingFallback;
+  if (error) return renderError(error);
+  if (data) return renderSuccess({ data });
+
+};
+
+export default Fetch;
