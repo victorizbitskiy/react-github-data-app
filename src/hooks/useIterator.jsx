1f1575daccff7466c30f432bfc8ded6ffc727a5f
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-08-17 20:14:29 +0300

    add usecalback to useiterator

--- src/hooks/useIterator.jsx
+++ src/hooks/useIterator.jsx
@@ -1,19 +1,21 @@
-import React, { useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 
-const useIterator = (items = [], initialIndex = 0) => {
-  const [index, setIndex] = useState(initialIndex);
+const useIterator = (items = [], initialValue = 0) => {
+  const [index, setIndex] = useState(initialValue);
 
-  const prev = () => {
+  const prev = useCallback(() => {
     if (index === 0) return setIndex(items.length - 1);
     setIndex(index - 1);
-  }
+  }, [index]);
 
-  const next = () => {
+  const next = useCallback(() => {
     if (index === items.length - 1) return setIndex(0);
     setIndex(index + 1);
-  }
+  }, [index]);
 
-  return [items[index], prev, next];
+  const item = useMemo(() => items[index], [index]);
+
+  return [item || items[index], prev, next];
 };
 
 export default useIterator;
